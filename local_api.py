8d7450c1c58fd57dc542e30494bf515d1b670635
Cameron Ashby <cashb15@wgu.edu>
2024-10-23 19:07:55 -0600

    Finished API step

--- local_api.py
+++ local_api.py
@@ -1,14 +1,15 @@
 import json
-
 import requests
 
-# TODO: send a GET using the URL http://127.0.0.1:8000
-r = None # Your code here
+# Send a GET using the URL http://127.0.0.1:8000
+url = "http://127.0.0.1:8000"
+r = requests.get(url)
 
-# TODO: print the status code
-# print()
-# TODO: print the welcome message
-# print()
+# Print the status code
+print("GET Status Code:", r.status_code)
+
+# Print the welcome message
+print("GET Response:", r.json())
 
 
 
@@ -29,10 +30,12 @@ data = {
     "native-country": "United-States",
 }
 
-# TODO: send a POST using the data above
-r = None # Your code here
+# Send a POST using the data above
+post_url = "http://127.0.0.1:8000/data/"
+r = requests.post(post_url, json=data)
 
-# TODO: print the status code
-# print()
-# TODO: print the result
-# print()
+# Print the status code
+print("POST Status Code:", r.status_code)
+
+# Print the result
+print("POST Result:", r.json())
